81be398ebae15a3149a2829ae992496c6928b88b
anmol bhandari <apb296@nyu.edu>
2012-08-20 16:42:20 -0400

    updated code for plotting parallel simulations

--- 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/PlotParallelSimulations.m
+++ 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/PlotParallelSimulations.m
@@ -1,45 +1,52 @@
-% This script runs K simulations of length NumSim with different choices of
-% starting value of initial difference in assets. 
+% This script plots the long simulation using data stored in
+% Data/SimDataParallel.mat file. 
 
 close all
 clear all
-%% Set the Parallel Config
-err=[];
-try
-    matlabpool
-catch err
-end
-if isempty(err)
-    
-    
-    if(matlabpool('size') > 0)
-        matlabpool close
-    end
-    
-    matlabpool open local;
-    
-end
-  
 
-% LOAD THE COEFF
-LastIter=250;
-load(['Data/c' num2str(LastIter) '.mat'])
-SetParaStruc
-%
-K=6;
-btild0grid=linspace(-1.5,1.5,K);
-NumSim=5000;
+load( 'Data/SimDataParallel.mat')
+mkdir ('Graphs/LongSimulations/FallingTaxes/');
+mkdir ('Graphs/LongSimulations/RisingTaxes/');
 
-%for ctrb=1:K
-%[sHist(:,ctrb),u2btildHist(:,ctrb),RHist(:,ctrb),TauHist(:,ctrb),YHist(:,ctrb),TransHist(:,ctrb),btildHist(:,ctrb)]=RunSimulations(2,LastIter,btild0grid(ctrb),NumSim,Para);
-%end
-%save( [Para.datapath 'SimDataParallel.mat'],'sHist','u2btildHist','RHist','TauHist','YHist','TransHist','btildHist')
-load( [Para.datapath 'SimDataParallel.mat'])
-% 
+% ---- CHANGE THIS AS PER THE CASE ---
+plotpath='Graphs/LongSimulations/RisingTaxes';
+% ----
+
+
+K=size(u2btildHist,2);
+IndxBenchMark=find(btild0grid==0); % find the index for the case u2btild=0, this is the benchmark case
+figure()
+subplot(2,1,1)
+plot(TauHist(:,IndxBenchMark))
+xlabel('t')
+ylabel('$\tau$','Interpreter','Latex')
+title('Labor taxes')
+subplot(2,1,2)
+plot(btildHist(:,IndxBenchMark))
+xlabel('t')
+ylabel('$\tilde{b}_2$','Interpreter','Latex')
+title('Relative assets of Agent 2')
+print(gcf,'-dpng',[plotpath 'SimulationsInitBtild_0.png'])
+
  figure()
- for ctrb=1:K
+ for ctrb=1:6
  subplot(3,2,ctrb)
  plot(TauHist(:,ctrb))
+ xlabel('t')
+ylabel('$\tau$','Interpreter','Latex')
+title(['$\tilde{b}_0=$' num2str(btild0grid(ctrb)) ],'Interpreter','Latex')
  end
+print(gcf,'-dpng',[plotpath 'SimulationsTau_all.png'])
 
 
+ figure()
+ for ctrb=1:6
+ subplot(3,2,ctrb)
+ plot(btildHist(:,ctrb))
+ xlabel('t')
+ylabel('$\tilde{b}_2$','Interpreter','Latex')
+title(['$\tilde{b}_0=$' num2str(btild0grid(ctrb)) ],'Interpreter','Latex')
+ end
+ print(gcf,'-dpng',[plotpath 'SimulationsBtild_all.png'])
+ 
+ 
