40f4bc7fcd5fa87544c35db8700cfde7d8ad8e6a
anmol bhandari <apb296@nyu.edu>
2012-08-27 18:30:51 -0400

    changed the name to FallingTaxes

--- 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/PlotParallelSimulations.m
+++ 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/PlotParallelSimulations.m
@@ -12,8 +12,8 @@ mkdir ('Graphs/LongSimulations/RisingTaxes/');
 mkdir ('Tex/LongSimulations/RisingTaxes/');
 
 % ---- CHANGE THIS AS PER THE CASE ---
-plotpath='Graphs/LongSimulations/RisingTaxes/';
-texpath='Tex/LongSimulations/RisingTaxes/';
+plotpath='Graphs/LongSimulations/FallingTaxes/';
+texpath='Tex/LongSimulations/FallingTaxes/';
 % ----
 
 
@@ -95,31 +95,27 @@ Moments(8,4)=corr(IntHist(end*BurnSampleRatio:end,IndxBenchMark),gHist(end*BurnS
 
 rowLabels = {'Shock : $g$','Labor Taxes: $\tau$', 'Transfers :$T$','Relative assets  :$\tilde{b}_2$', 'After-tax income (Agent 1)','After-tax income (Agent 2)','Income from assets (Agent 1)','Gross Int. rates'};
 columnLabels = {'Mean','Std','AutoCorr','Corr with g'};
-matrix2latex(Moments, [texpath 'SimulationMoments.tex'] , 'rowLabels', rowLabels, 'columnLabels', columnLabels, 'alignment', 'c', 'format', '%-6.2f', 'size', 'tiny');
+matrix2latex(Moments, [texpath 'SimulationMoments.tex'] , 'rowLabels', rowLabels, 'columnLabels', columnLabels, 'alignment', 'c', 'format', '%-6.4f', 'size', 'tiny');
 
 
 figure()
 subplot(3,1,1)
 plot(AfterTaxWageIncome_Agent1Hist(2:end,IndxBenchMark))
 xlabel('t')
-ylabel('$(1-\tau)\theta_1 l_1+T$','Interpreter','Latex')
 title('After Tax income (Agent1)')
 subplot(3,1,2)
 plot(AfterTaxWageIncome_Agent2Hist(2:end,IndxBenchMark))
 xlabel('t')
-ylabel('$(1-\tau)\theta_2 l_2+T$','Interpreter','Latex')
 title('After Tax income (Agent2)')
 
 subplot(3,1,3)
 plot(IncomeFromAssets_Agent1Hist(1:end,IndxBenchMark))
 xlabel('t')
-ylabel('$-\tilde{b}_2R(s\_,s)$','Interpreter','Latex')
 title('Income from Assets (Agent1)')
 print(gcf,'-dpng',[plotpath 'SimulationsIncome.png'])
 
 figure()
 hist(IntHist(1:end,IndxBenchMark))
-ylabel('$R$','Interpreter','Latex')
 title('Interest Rates')
 print(gcf,'-dpng',[plotpath 'SimulationsInterest.png'])
 
--- 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/PlotSimul.m
+++ 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/PlotSimul.m
@@ -10,7 +10,7 @@ Inx=find(X.sHist(1:T)<2);
     for i=2:length(Inx)-1
         bbT{i}=Inx(i)-1:Inx(i);
     end
-plot((1:T),X.Data(1:T))
+plot((1:T),X.Data(1:T),'k','LineWidth',1.5)
 axis tight
 ShadePlotForEmpahsis( bbT,'r',.05);  
 ylabel(X.name,'Interpreter','Latex')
