7f81001886f6b0b7e419327961278a3c3e94286d
anmol bhandari <apb296@nyu.edu>
2012-08-31 13:04:52 -0400

    added a file to run the MainBellma

---
+++ 2011RA/FiscalPolicy/GolosovProjectCode/Tom Example/BGP/RunMain.m
@@ -0,0 +1,7 @@
+ % This scrip computes the bellman equation for different values of theta1
+ clc
+ clear all
+ SetParaStruc;
+ Para.datapath=['Data/CompStats/'];
+ mkdir(Para.datapath)
+ Para.StoreFileName=['ctest'.mat'];
